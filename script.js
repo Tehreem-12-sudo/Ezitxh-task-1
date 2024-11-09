b84f837e9e2d8201e9417ece6a760ce542add93b
Tehreem-12-sudo <tehreemshafique370@gmail.com>
2024-11-10 04:53:58 +0500

    first Commit

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-$(document).ready(function() {
-    $('.slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: $('.slick-prev'),  // Link to your custom 'Previous' button
-        nextArrow: $('.slick-next')   // Link to your custom 'Next' button
-    });
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+// Auto Slide   if you need auto slide ,remove the commit "//"
+//var slideIndex = 0;
+//showSlides();
+//function showSlides() {
+    //var i;
+  //  var slides = document.getElementsByClassName("mySlides");
+   // for (i = 0; i < slides.length; i++) {
+   //     slides[i].style.display = "none";
+   // }
+  //  slideIndex++;
+   // if (slideIndex > slides.length) { slideIndex = 1 }
+   // slides[slideIndex - 1].style.display = "block";
+   // setTimeout(showSlides, 2000); // Change image every 2 seconds
+//}
